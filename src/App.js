438f5026a71451dd7a62d900a0337158bc5d5066
adamyaui <adamya.shukla@aptusdatalabs.com>
2023-10-07 15:10:48 +0530

    fixed line issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Disperse from './Disperse'; // Import the Disperse component
-import './App.css'; // You can import your global CSS styles here if needed
+import './App.css'; // 
 
 function App() {
   return (
     <div className="App">
-      <Disperse /> {/* Use the Disperse component */}
+      <Disperse /> 
     </div>
   );
 }
